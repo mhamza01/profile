398812b60c462277b3544573a4f13b35f1ce78ba
mhamza01 <114899765+mhamza01@users.noreply.github.com>
2022-12-14 01:27:44 -0400

    with javascript and Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/Header.js';
 import './App.css';
 
+
 function App() {
+  
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
+
+
+
+
   );
+}
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from 'react'
+
+const Header = () => {
+
+  
+
+
+
+
+  return (
+    <section>
+        <body>
+          <nav class="navbar" id="navbar">
+            <div class="brand-title">Hamza Ali</div>
+            <a href="#" class="toggle-button">
+              <span class="bar"></span>
+              <span class="bar"></span>
+              <span class="bar"></span>
+            </a>
+
+
+            <div class="navbar-links">
+              <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </nav>
+
+          <div class="content">
+            Teststst
+          </div>
+
+
+
+        </body>
+
+        
+    </section>
+  )
+}
+
+
+
+export default Header
+
