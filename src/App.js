ce6f62943b9e4f1390c9f66038a47b36c4053777
mhamza01 <114899765+mhamza01@users.noreply.github.com>
2022-12-14 01:32:19 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -20,22 +20,4 @@ function App() {
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
-
-// Get the header
-var header = document.getElementById("navbar");
-
-// Get the offset position of the navbar
-var sticky = header.offsetTop;
-
-// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
 export default App;
